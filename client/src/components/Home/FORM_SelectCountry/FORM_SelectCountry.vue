e8bce51b6f9d5635437e494292d7702fee662d46
LeoCosta001 <leocosta.black@gmail.com>
2020-04-15 17:03:54 -0300

    ATT Separção do SCSS e JS do componente FORM_SelectCountry / ATT Cor de fundo da Mensagem de erro da tabela

--- client/src/components/Home/FORM_SelectCountry/FORM_SelectCountry.vue
+++ client/src/components/Home/FORM_SelectCountry/FORM_SelectCountry.vue
@@ -15,102 +15,13 @@
   </div>
 </template>
 
-<script>
-import { http } from "@/axiosConfig/axiosConfig";
-import casesList from "@/axiosConfig/countryList";
-
-export default {
-  name: "FORM_SelectCountry",
-  data() {
-    return {
-      // Variaveis
-      countrySelected: "undefined",
-      countryList: false,
-      countryData: [],
-      countryDataLoading: false,
-      countryDataErro: false,
-
-      // Rota de requisição de dados para o back-end
-      casesDataReq: () => {
-        return http.get(`country-data/?country_select=${this.countrySelected}`);
-      }
-    };
-  },
-  computed: {
-    // Dados para serem emitidos para outros componentes
-    FORM_SelectCountryEmit() {
-      return {
-        countrySelected: this.countrySelected,
-        countryData: this.countryData,
-        countryDataLoading: this.countryDataLoading,
-        countryDataErro: this.countryDataErro
-      };
-    }
-  },
-  methods: {
-    // Emitir dados que estiverem no data "FORM_SelectCountryEmit" 
-    localEmit() {
-      this.$emit("resCountryData", this.FORM_SelectCountryEmit);
-    },
-
-    // Método acionado quando é selecionado um novo país
-    async casesData() {
-      // Ativar variável de "Carregamento" e desativar variável de "Erro"
-      this.countryDataLoading = true;
-      this.countryDataErro = false;
-      this.localEmit();
-      // Fazendo requisição
-      await this.casesDataReq()
-        .then(res => {
-          // Verificando retorno de dados
-          if (res.data.resError) {
-            this.countryDataLoading = false;
-            this.countryDataErro = true;
-            this.localEmit();
-          } else {
-            this.countryDataLoading = false;
-            this.countryData = res.data.countryCases;
-            this.localEmit();
-          }
-        })
-        .catch(err => {
-          console.log(err);
-
-          // Desativando variável de "Carregamento" e ativando variável de "Erro"
-          this.countryDataLoading = false;
-          this.countryDataErro = true;
-          this.localEmit();
-        });
-        
-      this.localEmit();
-    }
-  },
-  mounted() {
-    // Lista do nome dos países
-    casesList.toList().then(res => {
-      this.countryList = res.data;
-    });
-  }
-};
+<script src="./FORM_SelectCountry.js">
 </script>
 
 <style scoped lang="scss">
+// Variáveis
 @import "@/sass/variables/_flatUiColors";
 
-.localContainer {
-  display: flex;
-  flex-direction: column;
-}
-
-form {
-  align-self: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  & select {
-    background-color: $flatLightGrey;
-    padding: 10px;
-  }
-}
+// SCSS deste componente
+@import "./FORM_SelectCountry";
 </style>
