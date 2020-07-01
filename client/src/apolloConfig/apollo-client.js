7bba93ba4552a39d967670b1942499cb447cec83
Leocostablack <leocosta.black@gmail.com>
2020-07-01 12:27:55 -0300

    ATT Rota de acesso do graphql

--- client/src/apolloConfig/apollo-client.js
+++ client/src/apolloConfig/apollo-client.js
@@ -4,7 +4,7 @@ import { HttpLink } from "apollo-link-http";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: "http://localhost:3000/graphql"
+  uri: "/graphql"
 });
 
 export const client = new ApolloClient({
